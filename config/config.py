73b8fdd1257cf56aee6d2f93a6e14fa15643c78a
AlexeyZh <waltafunk@gmail.com>
2024-09-21 21:24:19 +0300

    chore(config): add config.py with base URLs and default timeout

---
+++ config/config.py
@@ -0,0 +1,9 @@
+BASE_URL = "https://www.globalsqa.com/angularJs-protractor/BankingProject/#"
+ADD_CUSTOMER_URL = "/manager/addCust"
+CUSTOMER_LIST_URL = "/manager/list"
+
+# Page titles
+XYZ_BANK_TITLE = "XYZ Bank"
+
+# Timeouts
+DEFAULT_TIMEOUT = 10
