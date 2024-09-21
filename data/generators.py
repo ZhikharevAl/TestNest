633247313be018e6fcdd8ad7259186a80a76bd0f
AlexeyZh <waltafunk@gmail.com>
2024-09-22 00:21:56 +0300

    refactor(generators): simplify docstring in create_customers for better readability

--- data/generators.py
+++ data/generators.py
@@ -2,16 +2,5 @@ from data.customer import Customer
 
 
 def create_customers(num_customers: int) -> list:
-    """
-    Create a list of customers.
-
-    This function generates a specified number of Customer objects
-    and returns them in a list.
-
-    Args:
-        num_customers (int): The number of customers to create.
-
-    Returns:
-        list: A list of Customer objects.
-    """
+    """Create a list of customers."""
     return [Customer() for _ in range(num_customers)]
