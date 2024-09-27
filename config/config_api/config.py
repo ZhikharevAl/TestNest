8beb2d4683baf517ed990438b14ecef6cd5e83f0
AlexeyZh <waltafunk@gmail.com>
2024-09-27 20:01:20 +0300

    chore: timeout is set in config from http_client

--- config/config_api/config.py
+++ config/config_api/config.py
@@ -1,6 +1,2 @@
-BASE_URL = "https://8a55-95-32-200-143.ngrok-free.app"  # change to your ngrok url
-CREATE_ENDPOINT = "/api/create"
-GET_ENDPOINT = "/api/get/"
-GET_ALL_ENDPOINT = "/api/getall/"
-UPDATE_ENDPOINT = "/api/patch/"
-DELETE_ENDPOINT = "/api/delete/"
+BASE_URL = "http://localhost:8080"  # change to your ngrok url
+TIMEOUT = 10
