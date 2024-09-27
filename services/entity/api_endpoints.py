6ad7d9483d61d52b8b02a79a79242f6d662209db
AlexeyZh <waltafunk@gmail.com>
2024-09-27 20:03:37 +0300

    feat: added endpoints for the api

---
+++ services/entity/api_endpoints.py
@@ -0,0 +1,7 @@
+class APIEndpoints:
+    """This class contains the API endpoints for the service."""
+    CREATE_ENDPOINT = "/api/create"
+    GET_ENDPOINT = "/api/get/"
+    GET_ALL_ENDPOINT = "/api/getall/"
+    UPDATE_ENDPOINT = "/api/patch/"
+    DELETE_ENDPOINT = "/api/delete/"
