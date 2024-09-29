0113d6faaae6b115234d0580c35b2a36a8d02290
AlexeyZh <waltafunk@gmail.com>
2024-09-29 20:30:29 +0300

    refactor: updated fixture

--- conftest.py
+++ conftest.py
@@ -4,6 +4,8 @@ import pytest
 
 from config.config_api.config import BASE_URL
 from services.entity.api_client import APIClient
+from services.entity.entity_service import EntityService
+from services.entity.models.entity_model import EntityResponse
 
 
 @pytest.fixture
@@ -13,11 +15,28 @@ def api_client() -> APIClient:
 
 
 @pytest.fixture
-def entity_id(api_client: APIClient) -> str:
-    """Fixture to create and yield a new entity ID."""
-    entity_id = api_client.create_entity().text
-    yield entity_id
-    api_client.delete_entity(entity_id)
+def entity_service(api_client: APIClient) -> EntityService:
+    """Fixture for creating an EntityService instance."""
+    return EntityService(api_client)
+
+
+@pytest.fixture
+def create_entity(entity_service: EntityService) -> EntityResponse:
+    """Fixture to create a new entity and return its data without deleting."""
+    _, entity = entity_service.create_entity()
+    return entity
+
+
+@pytest.fixture
+def create_and_delete_entity(entity_service: EntityService) -> EntityResponse:
+    """Fixture to create a new entity, yield it, and then delete it."""
+    _, entity = entity_service.create_entity()
+    yield entity
+    try:
+        entity_service.delete_entity(str(entity.id))
+    except Exception as e:
+        error_message = f"Failed to delete entity {entity.id}: {e!s}"
+        logging.exception(error_message)
 
 
 def pytest_configure() -> None:
